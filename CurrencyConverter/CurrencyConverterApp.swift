a62444b7bee564fb7e973ab07cf2adb7f681dba2
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-11-06 09:46:29 +0100

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Nadheer on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterTests.swift
+//  CurrencyConverterTests
+//
+//  Created by Nadheer on 06/11/2024.
+//
+
+import Testing
+@testable import CurrencyConverter
+
+struct CurrencyConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
